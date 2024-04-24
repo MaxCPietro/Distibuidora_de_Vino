f3d581ac0c66b0e67872df2343ea810bd2b7d96a
Juan Cruz Pérez <juanceprez@gmail.com>
2024-04-24 10:25:50 -0300

    Animacion de slider de clientes
    agregado de los estilos, logica y marcado para el funcionamiento del slider de clientes

--- js/index.js
+++ js/index.js
@@ -3,3 +3,32 @@ const h = 5-2;
 const j = 2*5;
 const l = 5/2;
 console.log(k,h,j,l)
+
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+// if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+//   addAnimation();
+// }
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+addAnimation();

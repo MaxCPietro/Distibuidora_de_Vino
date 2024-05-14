c750a2371cb7fa83fe4d3df67703d5307d453d8f
Juan Cruz Pérez <juanceprez@gmail.com>
2024-05-14 10:30:27 -0300

    pequenos cambios

--- js/validation.js
+++ js/validation.js
@@ -4,16 +4,17 @@ const correo = document.getElementById("email");
 const telefono = document.getElementById("tel");
 const mensaje = document.getElementById("mensaje");
 const terminos = document.getElementById("terminos");
+const alertNombre = document.querySelector('.alert-nombre');
 //primer rudimiento de validacion, falta trabajo de hacerlo mas funcional. Por el momento solo valida longitud del texto ingresado, estoy trabajando en la validacion de tipo
 nombre.addEventListener("blur", () => {
   let nombreValue = nombre.value;
   let length = nombreValue.length;
   if (nombreValue === "") {
-    alert("campo vacio");
+    alert("el campo no puede estar vacio");
   } else if (length > 20) {
-    alert("demasiado largo");
+    alert("el campo no puede contener mas de 20 caracteres");
   } else if (length < 3) {
-    alert("demasiado corto");
+    alert("el campo no puede contener menos de 3 caracteres");
   }
 });
 
@@ -21,11 +22,11 @@ empresa.addEventListener("blur", () => {
   let empresaValue = empresa.value;
   let length = empresaValue.length;
   if (empresaValue === "") {
-    alert("campo vacio");
+    alert("el campo no puede estar vacio");
   } else if (length > 20) {
-    alert("demasiado largo");
+    alert("el campo no puede contener mas de 20 caracteres");
   } else if (length < 3) {
-    alert("demasiado corto");
+    alert("el campo no puede contener menos de 3 caracteres");
   }
 });
 
@@ -33,11 +34,11 @@ correo.addEventListener("blur", () => {
   let correoValue = correo.value;
   let length = correoValue.length;
   if (correoValue === "") {
-    alert("campo vacio");
+    alert("el campo no puede estar vacio");
   } else if (length > 20) {
-    alert("demasiado largo");
+    alert("el campo no puede contener mas de 20 caracteres");
   } else if (length < 3) {
-    alert("demasiado corto");
+    alert("el campo no puede contener menos de 3 caracteres");
   }
 });
 
@@ -45,11 +46,11 @@ telefono.addEventListener("blur", () => {
   let telefonoValue = telefono.value;
   let length = telefonoValue.length;
   if (telefonoValue === "") {
-    alert("campo vacio");
+    alert("el campo no puede estar vacio");
   } else if (length > 20) {
-    alert("demasiado largo");
+    alert("el campo no puede contener mas de 20 caracteres");
   } else if (length < 10) {
-    alert("demasiado corto");
+    alert("el campo no puede contener menos de 10 caracteres");
   }
 });
 
@@ -57,10 +58,10 @@ mensaje.addEventListener("blur", () => {
   let mensajeValue = mensaje.value;
   let length = mensajeValue.length;
   if (mensajeValue === "") {
-    alert("campo vacio");
-  } else if (length > 2) {
-    alert("demasiado largo");
-  } else if (length < 1000) {
-    alert("demasiado corto");
+    alert("el campo no puede estar vacio");
+  } else if (length > 1000) {
+    alert("el campo no puede contener mas de 1000 caracteres");
+  } else if (length < 5) {
+    alert("el campo no puede contener menos de 5 caracteres");
   }
 });
